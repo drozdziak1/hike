9cf89dee619c6491c96583757c171a972cd988e5
Stan Drozd <drozdziak1@gmail.com>
2021-04-06 01:28:40 +0200

    Update niv sources, fix default.nix typo

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let
   rustOverlay = import "${nixpkgsMozilla}/rust-overlay.nix";
   cargo2nixOverlay = import "${cargo2nix}/overlay";
 
-  pkgs = import <nixpkgs> {
+  pkgs = import nixpkgs {
     inherit system;
     overlays = [ cargo2nixOverlay rustOverlay ];
   };
